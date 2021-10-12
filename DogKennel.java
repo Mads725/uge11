6a0d9200433bd04ecf16b6458a0ff04c3fd98385
tessG <youremail@example.com>
2021-10-12 18:10:46 +0200

    ...

--- DogKennel.java
+++ DogKennel.java
@@ -1,31 +1,30 @@
 /*
 FORMÅL: At kunne redesigne ved at skifte standard datatyper ud med egne klasser
 OPGAVEN: Der er fem delopgaver.
+
 1) I main skal man lave et hundeobjekt med navn og ejer
-2) Modificér hunde-klassen så den kan fodres. Den skal derfor have en boolean 
-attribut - isHungry - som skal initialiseres til true. Lav en metode - feedDog - 
-som tager hensyn til om hunden er sulten. Husk at sætte hunden til mæt når den  
-er blevet fodret.
+2) Skriv en klasse, Dog med en boolean attribut - isHungry - som skal initialiseres til true. 
+Lav en metode - feedDog - som tager hensyn til om hunden er sulten. 
+Husk at sætte isHungry til false, når den er blevet fodret.
 3) Modificér hunde-klassen så den kan få hvalpe. Dvs indfør en attribut a la:
-private String[] offSpring;
-Konstruktøren skal initialisere offSpring med en bestemt længde.
-Tilføj nu en getter og en setter. Setter-metoden skal sørge for at fylde
-offSpring fra neden - dvs tjek hvor de tomme pladser starter. Dernæst skal den
-også tage højde for at hunden faktisk kan få flere hvalpe end først antaget i
-konstruktøren (så noget med at bruge Arrays.copyOf-metoden hvis det oprindelige
-array er fyldt op)
-EXTRA OPGAVE 
-4) Ændre i hunde-klassen så ejeren ikke længere er type String men typen Owner
+private ArrayList<String> offSpring;
+Konstruktøren skal initialisere offSpring.
+4) Tilføj nu en getter og en setter. Setter-metoden skal sørge for at fylde
+offSpring til Dog klassen.
+5) Modificer  Dog klassen så ejeren ikke længere er type String men typen Owner
 (som altså er en klasse der skal laves fra bunden). Dvs flg skal skiftes:
 private String ejer;
 så det bliver til:
 private Owner ejer;
 Når det er ændret vil man også skulle ændre i getter og setter samt i main.
 Flg skal kunne virke fra main:
-System.out.println("hunden " + getHundeNavn() + " er ejet af " + myDog.getOwner());
-5) Gør det samme for offSpring. Dvs
-private String[] offSpring; bliver til
-private Dog[] offSpring;
+System.out.println("hunden " + getDogName() + " er ejet af " + myDog.getOwner());
+6) Gør det samme for offSpring. Dvs
+private ArrayList<String> offSpring; bliver til
+private ArrayList <Dog> offSpring;
+7) lav en metode der kan printe navnene på en hund's hvalpe.
+
+
 */
 import java.util.Arrays;
 public class DogKennel {
@@ -40,11 +39,11 @@ public class DogKennel {
 		myDog.setOffSpring("woodiesfido");
 		
 		// print alle hvalpe
-		String[] myOff = myDog.getOffSpring();
+		ArrayList <String> myOff = myDog.getOffSpring();
 		for (String myD: myOff) {
 			System.out.println(myD);
 		}
-		System.out.println(Arrays.toString(myOff));
+		
 		// print ejeren
 		System.out.println("min hund er ejet af " + myDog.getOwner());
 		// TODO: fodr hunden når hundeklassen er modificeret så den kan fodres
